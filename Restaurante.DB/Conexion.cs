cfc26a4db7e3dac703ab424f0774ed1def583288
Nicolas Olivares <nicolasolivares097@gmail.com>
2022-01-12 02:26:55 -0300

    Se configuró la conexión

--- Restaurante.DB/Conexion.cs
+++ Restaurante.DB/Conexion.cs
@@ -1,13 +1,126 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using System.IO;
 
 namespace Restaurante.DB
 {
     public  class Conexion
     {
-        
+        static public (string, string, string) CredencialesBD()
+        {
+            string filepath = "config.ini";
+            string _user = File.ReadAllLines(filepath)[0].Split('=')[1].Trim();
+            string _pwd = File.ReadAllLines(filepath)[1].Split('=')[1].Trim();
+            string _db = File.ReadAllLines(filepath)[2].Split('=')[1].Trim();
+
+
+            return (_user, _pwd, _db);
+        }
+
+        static string user = CredencialesBD().Item1;
+        static string pwd = CredencialesBD().Item2;
+        static string db = CredencialesBD().Item3;
+
+        static OracleConnection oracleConnection = new("User Id=" + user + ";Password=" + pwd + ";Data Source=" + db + ";");
+        static OracleCommand oracleCommand = oracleConnection.CreateCommand();
+
+        public OracleConnection OracleConnection { get => oracleConnection; set => oracleConnection = value; }
+        public OracleCommand OracleCommand { get => oracleCommand; set => oracleCommand = value; }
+
+
+
+
+        public bool CheckDatabase()
+        {
+
+            if (OracleConnection.State == ConnectionState.Open)
+            {
+                return true;
+            }
+            else
+            {
+                try
+                {
+                    OracleConnection.Open();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+            }
+
+
+        }
+        public DataTable OracleToDataTable(string sqlcommand)
+        {
+
+            DataTable tb = new();
+            if (CheckDatabase())
+            {
+                OracleDataReader reader = RunOracleExecuteReader(sqlcommand);
+                tb.Load(reader);
+                return tb;
+
+            }
+
+            return tb;
+        }
+
+        public object RunOracleExecuteScalar(string sqlcommand)
+        {
+            //Devuelve un solo objeto
+            object newobj = null;
+            if (CheckDatabase())
+            {
+                try
+                {
+                    OracleCommand.CommandText = sqlcommand;
+
+                    newobj = OracleCommand.ExecuteScalar();
+                }
+                catch (InvalidOperationException)
+                {
+
+                    return newobj;
+
+                }
+
+
+            }
+
+            return newobj;
+        }
+
+        public OracleDataReader RunOracleExecuteReader(string sqlcommand)
+        {
+            if (CheckDatabase())
+            {
+                OracleCommand.CommandText = sqlcommand;
+                OracleDataReader odr = OracleCommand.ExecuteReader();
+                return odr;
+
+            }
+
+            return null;
+        }
+
+        public void RunOracleNonQuery(string sqlcommand)
+        {
+            if (CheckDatabase())
+            {
+                OracleCommand.CommandText = sqlcommand;
+                OracleCommand.ExecuteNonQuery();
+
+
+            }
+
+
+        }
+
+
     }
+
 }
